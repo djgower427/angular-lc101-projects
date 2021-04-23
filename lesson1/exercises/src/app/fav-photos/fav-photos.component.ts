d493eb9a3050778d9c4d1d5b6aacc16db1642adc
Dan Gower <gower.daniel.j@gmail.com>
2021-04-23 14:24:19 -0500

    did 28.7.3 part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Rad Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://uploads-ssl.webflow.com/5a568cc4c8be6900019872d4/5e7d7033eaf7ec302520e84e_BuddyGardnerLogoWhite%20(1)-p-500.png';
+  image3 = 'https://pbs.twimg.com/media/C_FFjVkUQAASQgC.jpg';
 
   constructor() { }
 
